f25fd72e14e468d2bcb31ea96c73d3d93de13bff
Madhav <maddy@dealsignal.com>
2024-08-20 18:45:08 +0530

    Added initial feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'dealsignal-18';
+export class AppComponent implements OnInit {
+  title = '';
+  profile: any;
+
+  constructor() {
+    chrome.storage.local.get(['siteData'], (result: any) => {
+      localStorage.setItem('siteData', JSON.stringify(result))
+    });
+
+    this.profile = null;
+  }
+
+  ngOnInit(): void {
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      if (message.title == 'fromBackground') {
+        console.log(message.info)
+        sendResponse(true);
+      }
+
+      if (message.title == 'refreshPage') {
+        chrome.tabs.sendMessage(message.windowId, { title: 'parsePage', tab: message.windowId, ceTabId: message.ceTabId })
+        sendResponse(true);
+      }
+
+      if (message.title == 'parsedData') {
+        console.log(message.payload)
+        this.profile = message.payload;
+      }
+    })
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    debugger
+  }
 }
